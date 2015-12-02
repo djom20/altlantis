1680b446737a85ace98970845acefd772b7f248e
Ing. Jonathan Olier <djom202@gmail.com>
2015-12-02 13:57:36 -0500

    Add PhpUnitTest lib

---
+++ test/UnitTest/IndexControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+		IndexControllerTest Class
+	**/
+
+	class IndexControllerTest extends PHPUnit_Framework_TestCase
+	{
+		public function testArrayEmpty()
+		{
+			$stack = array();
+			$this->assertEmpty($stack);
+
+			return $stack;
+		}
+	}
