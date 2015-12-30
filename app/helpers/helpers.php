68e12101cf87a636c8c0a75d32f23a5b7b3d5cab
Ing. Jonathan Olier <djom202@gmail.com>
2015-12-30 17:45:57 -0500

    Add support logger

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -1,22 +1,2 @@
 <?php
-	/**
-		Helper's Class
-	*
-	* @author Ing. Jonathan Olier djom202@gmail.com
-	*
-	*/
-
-	if(!class_exists('secret'))
-	{
-		function secret(){}
-	}
-
-	if(!class_exists('ask'))
-	{
-		function ask(){}
-	}
-
-	if(!class_exists('info'))
-	{
-		function info(){}
-	}
+	// Helpers Classes
